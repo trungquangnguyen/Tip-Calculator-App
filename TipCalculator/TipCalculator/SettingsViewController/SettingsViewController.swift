e4bf57dede2b715ee299144b537137b3f33d659a
nguyen trung quang <trunhquang@gmail.com>
2016-01-31 22:06:20 +0700

    add design for SettingsViewController and all function

--- TipCalculator/TipCalculator/SettingsViewController/SettingsViewController.swift
+++ TipCalculator/TipCalculator/SettingsViewController/SettingsViewController.swift
@@ -9,33 +9,152 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet weak var tfMin: UITextField!
+    @IBOutlet weak var tfMax: UITextField!
+    @IBOutlet weak var lblTipPerCent: UILabel!
+    @IBOutlet weak var lblMinTipPerCent: UILabel!
+    @IBOutlet weak var lblMaxTipPerCent: UILabel!
+    @IBOutlet weak var sliderTipPerCent: UISlider!
+    var minPerCent: Double = 0.0
+    var maxPerCent: Double = 0.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Settings"
+        self.navigationController?.navigationBar.translucent = false
         
         let leftButton = UIBarButtonItem(image: UIImage(named: "back_icon"), style: .Plain, target: self, action:"leftButtonAction:")
-        //        let rightButton = UIBarButtonItem(title: "Settings", style: .Plain, target: self, action:"rightButtonAction:")
+
         self.navigationItem.leftBarButtonItem = leftButton;
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        if(userDefaults.valueForKey("minPerCent") != nil){
+            sliderTipPerCent.minimumValue = (userDefaults.valueForKey("minPerCent")?.floatValue)!
+        }else{
+            sliderTipPerCent.minimumValue = 0.0
+        }
+        if(userDefaults.valueForKey("maxPerCent") != nil){
+            sliderTipPerCent.maximumValue = (userDefaults.valueForKey("maxPerCent")?.floatValue)!
+        }else{
+            sliderTipPerCent.maximumValue = 100.0
+        }
+        if(userDefaults.valueForKey("currentPerCent") != nil){
+            sliderTipPerCent.value        = (userDefaults.valueForKey("currentPerCent")?.floatValue)!
+        }else{
+            sliderTipPerCent.value = 0.0
+        }
+        
+        
+        lblTipPerCent.text = "%" + String(sliderTipPerCent.value)
+        lblMinTipPerCent.text = String(sliderTipPerCent.minimumValue)
+        lblMaxTipPerCent.text = String(sliderTipPerCent.maximumValue)
+        
+        minPerCent = Double(sliderTipPerCent.minimumValue)
+        maxPerCent = Double(sliderTipPerCent.maximumValue)
+        
+        tfMax.text = String(maxPerCent)
+        tfMin.text = String(minPerCent)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     func leftButtonAction(Sender:UIBarButtonItem){
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setValue(minPerCent, forKey: "minPerCent")
+        userDefaults.setValue(maxPerCent, forKey: "maxPerCent")
+        userDefaults.setValue(sliderTipPerCent.value  , forKey: "currentPerCent")
+        userDefaults.synchronize()
+//        NSNotificationCenter.defaultCenter().postNotificationName("popToRoot", object: String(sliderTipPerCent.value));
         self.navigationController?.popToRootViewControllerAnimated(true)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func beginEditingMinTextField(sender: AnyObject) {
+        if((tfMin.text) != nil){
+            tfMin.text = ""
+        }
     }
-    */
+    @IBAction func endEdittingMinTextField(sender: AnyObject) {
+        let value = NSNumberFormatter().numberFromString(tfMin.text!)
+        let min = NSNumberFormatter().numberFromString(tfMin.text!)
+        let max = Float(maxPerCent)
+        
+        if((value) == nil || max < min?.floatValue){
+            tfMin.text = String(minPerCent)
+        }else{
+            sliderTipPerCent.minimumValue = (value?.floatValue)!
+            minPerCent = Double(sliderTipPerCent.minimumValue)
+            lblMinTipPerCent.text = String(sliderTipPerCent.minimumValue)
+            
+            lblTipPerCent.text = "%" + String(sliderTipPerCent.value)
+            //            NSNotificationCenter.defaultCenter().postNotificationName("changeMinPerCent", object: nil);
+        }
+    }
+    @IBAction func changedMinTextField(sender: AnyObject) {
+        let value = NSNumberFormatter().numberFromString(tfMin.text!)
+        
+        if((value) != nil){
+            let _minPerCent = (value?.doubleValue)!
+            if(_minPerCent > 100.0 || _minPerCent < 0){
+                let alert = UIAlertController(title: "Error!", message: "Input again,Please", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+                
+                tfMin.text = ""
+            }
+        }else{
+            
+        }
+        
+    }
+    
+    @IBAction func beginEdittingMaxTextField(sender: AnyObject) {
+        if((tfMax.text) != nil){
+            tfMax.text = ""
+        }
+    }
+    @IBAction func endEdittingMaxTextField(sender: AnyObject) {
+        let value = NSNumberFormatter().numberFromString(tfMax.text!)
+        let min = Float(minPerCent)
+        let max = NSNumberFormatter().numberFromString(tfMax.text!)
+        
+        if((value) == nil || max?.floatValue < min){
+            tfMax.text = String(maxPerCent)
+        }else{
+            sliderTipPerCent.maximumValue = (value?.floatValue)!
+            maxPerCent = Double(sliderTipPerCent.maximumValue)
+            lblMaxTipPerCent.text = String(sliderTipPerCent.maximumValue)
+            
+            lblTipPerCent.text = "%" + String(sliderTipPerCent.value)
+            //             NSNotificationCenter.defaultCenter().postNotificationName("changeMaxPerCent", object: nil);
+        }
+    }
+    @IBAction func changeEditMaxValueTextField(sender: AnyObject) {
+        let value = NSNumberFormatter().numberFromString(tfMax.text!)
+        
+        if((value) != nil){
+            let _maxPerCent = (value?.doubleValue)!
+            if(_maxPerCent > 100.0 || _maxPerCent < 0){
+                let alert = UIAlertController(title: "Error!", message: "Input again,Please", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+                tfMax.text = ""
+            }
+        }else{
+            
+        }
+        
+    }
+ 
+    @IBAction func changedValueTipPerCent(sender: AnyObject) {
+        lblTipPerCent.text = "%" + String(sliderTipPerCent.value)
+//         NSNotificationCenter.defaultCenter().postNotificationName("changeTipPerCent", object: String(sliderTipPerCent.value));
 
+    }
+
+    @IBAction func tapOn(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
--- TipCalculator/TipCalculator/TipViewController/TipCalculatorViewController.swift
+++ TipCalculator/TipCalculator/TipViewController/TipCalculatorViewController.swift
@@ -10,34 +10,101 @@ import UIKit
 
 class TipCalculatorViewController: UIViewController {
 
+    @IBOutlet weak var lblTipAmount: UILabel!
+    
+    @IBOutlet weak var tfBillAmount: UITextField!
+    @IBOutlet weak var lblTotal: UILabel!
+    
+    @IBOutlet weak var lblTipPercent: UILabel!
+    
+    @IBOutlet weak var sliderTipPercent: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Tip Calculator"
         self.navigationController?.navigationBar.translucent = false;
         
         let rightButton = UIBarButtonItem(image: UIImage(named: "setting_icon"), style: .Plain, target: self, action:"rightButtonAction:")
-//        let rightButton = UIBarButtonItem(title: "Settings", style: .Plain, target: self, action:"rightButtonAction:")
         self.navigationItem.rightBarButtonItem = rightButton;
+        
+        tfBillAmount.adjustsFontSizeToFitWidth = true
+        lblTotal.adjustsFontSizeToFitWidth = true
+        lblTipAmount.adjustsFontSizeToFitWidth = true
+        
+        
+        
+        
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: "changeTipPerCent:", name:"changeTipPerCent", object: nil);
     }
 
+    override func viewWillAppear(animated: Bool) {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        if(userDefaults.valueForKey("minPerCent") != nil){
+            sliderTipPercent.minimumValue = (userDefaults.valueForKey("minPerCent")?.floatValue)!
+        }else{
+            sliderTipPercent.minimumValue = 0.0
+        }
+        if(userDefaults.valueForKey("maxPerCent") != nil){
+            sliderTipPercent.maximumValue = (userDefaults.valueForKey("maxPerCent")?.floatValue)!
+        }else{
+            sliderTipPercent.maximumValue = 100.0
+        }
+        if(userDefaults.valueForKey("currentPerCent") != nil){
+            sliderTipPercent.value        = (userDefaults.valueForKey("currentPerCent")?.floatValue)!
+        }else{
+            sliderTipPercent.value = 0.0
+        }
+        
+        lblTipPercent.text = "%" + String(sliderTipPercent.value)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        
     }
     
-
+    
+    //Notification
+//    func changeTipPerCent(notification:NSNotification){
+//        let string = String(notification.object)
+//        let double = NSNumberFormatter().numberFromString(string)?.doubleValue
+//        print(string)
+//    }
+    
+    //Action
     func rightButtonAction(sender:UIBarButtonItem){
         self.navigationController?.pushViewController(SettingsViewController(), animated: true)
     }
 
-    /*
-    // MARK: - Navigation
+    @IBAction func EditChangedTextField(sender: UITextField) {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        nf.maximumFractionDigits = 2
+        
+        lblTipPercent.text = "%" + String(sliderTipPercent.value)
+        let tipPercent = Double(sliderTipPercent.value)/100
+        
+        
+        let billAmount = NSNumberFormatter().numberFromString(sender.text!)
+        
+        if((billAmount) != nil){
+            let tip   = billAmount!.doubleValue * tipPercent
+            let total = billAmount!.doubleValue + tip
+            
+            lblTipAmount.text = "$"+nf.stringFromNumber(tip)!
+            lblTotal.text     = "$"+nf.stringFromNumber(total)!
+        }else{
+            lblTipAmount.text = "$ 0.0"
+            lblTotal.text  = "$ 0.0"
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+    @IBAction func sliderTipPercentChange(sender: AnyObject) {
+        self.EditChangedTextField(tfBillAmount);
+    }
+
+    @IBAction func tapOn(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
 }
