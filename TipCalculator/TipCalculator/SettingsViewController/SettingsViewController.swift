95a34cc235d5198ab0102bc48971b06c5f0294c5
nguyen trung quang <trunhquang@gmail.com>
2016-02-28 14:25:18 +0700

    update some Function and UI

--- TipCalculator/TipCalculator/SettingsViewController/SettingsViewController.swift
+++ TipCalculator/TipCalculator/SettingsViewController/SettingsViewController.swift
@@ -8,25 +8,84 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
+{
+    @IBOutlet weak var pickerTipView: UIPickerView!
+    @IBOutlet weak var pickerSplit: UIPickerView!
     
+    
+    var pickerTipDataSource: NSMutableArray = []
+    var pickerSplitDataSource: NSMutableArray = []
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        self.pickerTipView.dataSource = self
+        self.pickerTipView.delegate = self
+        
+        self.pickerSplit.dataSource = self
+        self.pickerSplit.delegate = self
+        
+        //set Data default for pickerView
+        for var i = 0; i<=50; i++ {
+            if(i == 0){
+                pickerSplitDataSource.addObject("no Split")
+                pickerTipDataSource.addObject("0% (Included)")
+            }else{
+                pickerSplitDataSource.addObject("\(i) People")
+                if(i == 5){
+                    pickerTipDataSource.addObject("\(i)% (Poor)")
+                }else if (i == 10){
+                    pickerTipDataSource.addObject("\(i)% (Fair)")
+                }else if (i == 15){
+                    pickerTipDataSource.addObject("\(i)% (Good)")
+                }else if (i == 20){
+                    pickerTipDataSource.addObject("\(i)% (Great)")
+                }else if (i == 25){
+                    pickerTipDataSource.addObject("\(i)% (Excellent)")
+                }else{
+                    pickerTipDataSource.addObject("\(i)%")
+                }
+            }
+        }
+        
+        // get from USER default
+        self.pickerTipView.selectRow(1, inComponent: 0, animated: true)
+        self.pickerSplit.selectRow(1, inComponent: 0, animated: true);
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func tapOn(sender: AnyObject) {
-        view.endEditing(true)
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 51;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if(pickerView .isEqual(pickerTipView)){
+            return pickerTipDataSource[row] as? String
+        }
+        return pickerSplitDataSource[row] as? String
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        if(pickerView.isEqual(pickerTipView)){
+            NSNotificationCenter.defaultCenter().postNotificationName("pickerTipView", object:nil, userInfo: ["message":"\(row)"] );
+        }else{
+            NSNotificationCenter.defaultCenter().postNotificationName("pickerSplit", object:nil, userInfo: ["message":"\(row)"]);
+        }
+    }
+
     @IBAction func disMissSelf(sender: AnyObject) {
-        self.navigationController?.popToRootViewControllerAnimated(false)
+        self.navigationController?.popToRootViewControllerAnimated(true)
     }
     override func prefersStatusBarHidden() -> Bool {
         return true
--- TipCalculator/TipCalculator/TipViewController/TipCalculatorViewController.swift
+++ TipCalculator/TipCalculator/TipViewController/TipCalculatorViewController.swift
@@ -9,22 +9,103 @@
 import UIKit
 
 class TipCalculatorViewController: UIViewController {
+//contraint for split
+    @IBOutlet weak var marginLeftContrain: NSLayoutConstraint!
+    @IBOutlet weak var split2_margin: NSLayoutConstraint!
+    @IBOutlet weak var split3_margin: NSLayoutConstraint!
+    @IBOutlet weak var split4_margin: NSLayoutConstraint!
+    @IBOutlet weak var split5_margin: NSLayoutConstraint!
+    @IBOutlet weak var splitmore_margin: NSLayoutConstraint!
+    
+//contraint for style Service
+    @IBOutlet weak var service2_margin: NSLayoutConstraint!
+    @IBOutlet weak var service3_margin: NSLayoutConstraint!
+    @IBOutlet weak var service4_margin: NSLayoutConstraint!
+    @IBOutlet weak var service5_margin: NSLayoutConstraint!
+    @IBOutlet weak var servicemore_margin: NSLayoutConstraint!
+    
+//contraint for check Amount
+    @IBOutlet weak var checkAmountMargin: NSLayoutConstraint!
+// split OutLlet
+    @IBOutlet weak var lblNumberSplit: UILabel!
+    @IBOutlet weak var imageSplit: UIImageView!
+    @IBOutlet weak var btnSplit_1: UIButton!
+    @IBOutlet weak var btnSplit_2: UIButton!
+    @IBOutlet weak var btnSplit_3: UIButton!
+    @IBOutlet weak var btnSplit_4: UIButton!
+    @IBOutlet weak var btnSplit_5: UIButton!
+    @IBOutlet weak var btnMoreSplit: UIButton!
+//style Service
+    @IBOutlet weak var lblPercentTip: UILabel!
+    @IBOutlet weak var imageStyleService: UIImageView!
+    @IBOutlet weak var btnService_1: UIButton!
+    @IBOutlet weak var btnService_2: UIButton!
+    @IBOutlet weak var btnService_3: UIButton!
+    @IBOutlet weak var btnService_4: UIButton!
+    @IBOutlet weak var btnService_5: UIButton!
+    @IBOutlet weak var btnServiceMore: UIButton!
+//Check Amount Outlet
+    @IBOutlet weak var lblAmount: UILabel!
+    @IBOutlet weak var btnAmount: UIButton!
+    @IBOutlet weak var imageAmount: UIImageView!
+    
+//__
+//
+    var arraySplit: NSArray = []
+    var arrayService: NSArray = []
+    var numberSplit: NSInteger = 0
+    var percentTip: NSInteger = 0;
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Tip Calculator"
+        
+        //set Autolayout for split Icon
+        let widthScreen = (UIScreen.mainScreen().bounds.size.width)
+        let titleMargin = (widthScreen - (31*5) - 44 )/7
+        let splitMargin = titleMargin
+        marginLeftContrain.constant = titleMargin
+        split2_margin.constant      = splitMargin
+        split3_margin.constant      = splitMargin
+        split4_margin.constant      = splitMargin
+        split5_margin.constant      = splitMargin
+        splitmore_margin.constant   = splitMargin
+        
+        //set Autolayout for style Service
+        let serviceMargin = (widthScreen - (40*5) - 44 - titleMargin)/6
+        service2_margin.constant = serviceMargin
+        service3_margin.constant = serviceMargin
+        service4_margin.constant = serviceMargin
+        service5_margin.constant = serviceMargin
+        servicemore_margin.constant = serviceMargin
+        
+        //set Autolayout for check Amount
+        checkAmountMargin.constant = titleMargin
+        
+        //set AllButton for array
+        arraySplit = [btnSplit_1,btnSplit_2,btnSplit_3,btnSplit_4,btnSplit_5,btnMoreSplit]
+        arrayService = [btnService_1,btnService_2,btnService_3,btnService_4,btnService_5,btnServiceMore]
+        
+        //registry receive Notification
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "receivedTip:", name:"pickerTipView", object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "receivedSplit:", name:"pickerSplit", object: nil);
+        
     }
 
     override func viewWillAppear(animated: Bool) {
 
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        NSNotificationCenter.defaultCenter().removeObserver(self);
         
     }
-    
+//_____
+//
     @IBAction func SettingButtonAction(sender: AnyObject) {
-        self.navigationController?.pushViewController(SettingsViewController(), animated: false)
+        self.navigationController?.pushViewController(SettingsViewController(), animated: true)
     }
     @IBAction func tapOn(sender: AnyObject) {
         view.endEditing(true)
@@ -32,4 +113,100 @@ class TipCalculatorViewController: UIViewController {
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
+//____
+//
+    @IBAction func splitAction(sender: AnyObject) {
+        numberSplit = sender.tag;
+        if((sender.tag) == 6){
+            self.navigationController?.pushViewController(SettingsViewController(), animated: true)
+        }else{
+            self.setSplit()
+        }
+    }
+    
+    @IBAction func styleServiceAction(sender: AnyObject) {
+        percentTip = sender.tag * 5;
+        if((sender.tag) == 6){
+            self.navigationController?.pushViewController(SettingsViewController(), animated: true)
+        }else{
+            self.setTip()
+        }
+    }
+    @IBAction func checkAmountAction(sender: AnyObject) {
+        print("sadas")
+    }
+//___receive Notification
+//
+    func receivedTip(notification: NSNotification){
+        let userInfo:Dictionary<String,String!> = notification.userInfo as! Dictionary<String,String!>
+        let messageString = userInfo["message"]
+        if let number = messageString {
+            self.percentTip = NSInteger(number)!
+            self.setTip()
+        }
+    }
+    func receivedSplit(notification: NSNotification){
+        let userInfo:Dictionary<String,String!> = notification.userInfo as! Dictionary<String,String!>
+        let messageString = userInfo["message"]
+        if let number = messageString {
+        self.numberSplit = NSInteger(number)!
+            self.setSplit()
+        }
+    }
+//___Private Method
+//
+    func setSelectedButton(index: NSInteger,array: NSArray){
+        for var i = 0;i < index;i++ {
+            var button = UIButton()
+            button = array[i] as! UIButton
+            button.selected = true;
+        }
+        for var i = index;i < 6;i++ {
+            var button = UIButton()
+            button = array[i] as! UIButton
+            button.selected = false;
+        }
+    }
+    
+    func setSplit() {
+        //setTextSplit()
+        if(numberSplit == 1){
+            lblNumberSplit.text = "";
+            imageSplit.image = UIImage(named: "text_no_split");
+        }else{
+            lblNumberSplit.text = String(numberSplit)
+            imageSplit.image = UIImage(named: "text_people_split");
+        }
+        //setButton Select Split
+        if(numberSplit >= 6){
+            self.setSelectedButton(6, array: arraySplit)
+        }else{
+            self.setSelectedButton(numberSplit, array: arraySplit)
+        }
+    }
+    func setTip(){
+        //setTextTip()
+        lblPercentTip.text = String(percentTip)
+        if(percentTip >= 25){
+            imageStyleService.image = UIImage(named: "text_excellent")
+        }else if(percentTip >= 20){
+            imageStyleService.image = UIImage(named: "text_great")
+        }else if(percentTip >= 15){
+            imageStyleService.image = UIImage(named: "text_good")
+        }else if(percentTip >= 10){
+             imageStyleService.image = UIImage(named: "text_fair")
+        }else if(percentTip >= 5){
+            imageStyleService.image = UIImage(named: "text_poor")
+        }else{
+            imageStyleService.image = UIImage(named: "text_included")
+        }
+        //setButton Select Tip
+        if(percentTip/5 >= 6){
+            self.setSelectedButton(6, array: arrayService)
+        }else{
+            self.setSelectedButton(percentTip/5, array: arrayService)
+        }
+    }
 }
+
+
